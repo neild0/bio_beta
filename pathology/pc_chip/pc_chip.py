ba42041a98d592840dc486e4771c791bbf1e802b
neild1 <neil.nitin.de@Gmail.com>
2021-06-05 15:39:16 -0400

    Added Base for PC-CHIP

---
+++ pathology/pc_chip/pc_chip.py
@@ -0,0 +1,5 @@
+from model import Model
+
+class PC_CHIP(Model):
+    def __init__(self, species: str = 'human'):
+        pass
---
+++ pathology/pc_chip/setup.sh
@@ -0,0 +1,2 @@
+wget -q -O model.zip https://www.ebi.ac.uk/biostudies/files/S-BSST292/u/Retrained_Inception_v4.zip && unzip model.zip && rm model.zip
+wget -q -O model.zip https://www.ebi.ac.uk/biostudies/files/S-BSST292/u/Retrained_Inception_v4_alt.zip && unzip model.zip && rm model.zip
