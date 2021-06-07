8a058ee01139b246da22e645a4d930e978c952fc
neild1 <neil.nitin.de@Gmail.com>
2021-06-07 14:55:59 -0400

    Cleaned Setup Files

--- pathology/pc_chip/setup.sh
+++ pathology/pc_chip/setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [[ "$2" = "og" ]]
-then
+if [[ "$2" == "og" ]]; then
   wget -q --show-progress -O ${1}/model/model.zip https://www.ebi.ac.uk/biostudies/files/S-BSST292/u/Retrained_Inception_v4.zip && unzip -q ${1}/model/model.zip -d ${1}/model && rm ${1}/model/model.zip
 
-elif [[ "$2" = "alt" ]]
-then
+elif [[ "$2" == "alt" ]]; then
   wget -q --show-progress -O ${1}/model/model.zip https://www.ebi.ac.uk/biostudies/files/S-BSST292/u/Retrained_Inception_v4_alt.zip && unzip -q ${1}/model/model.zip -d ${1}/model && rm ${1}/model/model.zip
 fi
