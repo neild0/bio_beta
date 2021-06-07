d798325bdba2a597bceca25f73ed9ce5b4a5de95
neild1 <neil.nitin.de@Gmail.com>
2021-06-07 14:53:56 -0400

    Updated Base ProtTrans Models

--- proteomics/protTrans/protTrans.py
+++ proteomics/protTrans/protTrans.py
@@ -4,6 +4,11 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, Text
 from base import Model
 from preproc_protTrans import PreProc_protTrans
 
+import sys
+import os
+import subprocess
+import pathlib
+
 
 class ProtTrans(Model):
     def __init__(self, models_dict=None):
@@ -11,6 +16,13 @@ class ProtTrans(Model):
         if models_dict is None:
             models_dict = {'MS': 'Rostlab/prot_bert_bfd_membrane', 'SS3': 'Rostlab/prot_bert_bfd_ss3',
                            'LCL': 'Rostlab/prot_bert_bfd_localization'}
+
+        if 'transformers' not in sys.modules:
+            curDir = pathlib.Path(__file__).parent.absolute()
+            print('Downloading Transfomer Files...')
+            os.chmod(f"{curDir}/setup.sh", 0o755)
+            subprocess.call([f"{curDir}/setup.sh"], stdout=sys.stdout, stderr=subprocess.STDOUT)
+
         self.MS_pipeline = TextClassificationPipeline(
             model=AutoModelForSequenceClassification.from_pretrained(models_dict['MS']),
             tokenizer=AutoTokenizer.from_pretrained(models_dict['MS']),
