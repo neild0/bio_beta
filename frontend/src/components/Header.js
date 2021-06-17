b56a1f125870e1a9c668b760c7a6ad0ff5c29a4c
neild1 <neil.nitin.de@Gmail.com>
2021-06-16 20:23:31 -0400

    Updating SearchBar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,21 +3,22 @@ import { Text, StyleSheet } from 'react-native';
 import axios from "axios";
 import { Row, Col, Space, Input, TextArea } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
+import './header.css'
 
 class Header extends React.Component {
     render() {
         return (
             <Row gutter={12}>
-                <Col span={4}>
+                <Col span={5}>
                     <span style={{position: "absolute", fontWeight: 1000, fontSize: 40, marginLeft:30, marginTop: 30}}>
                     Moonbear
                     </span>
                 </Col>
                 <Col span={12}>
-                    <Input
+                    <Input className="aaaa"
                         prefix={ <SearchOutlined />}
                         placeholder="    Search Moonbear..."
-                        style={{borderRadius: '30px', position: "absolute", fontSize: 30, marginLeft:50, marginTop: 35, backgroundColor:"#EEEEEE", }}
+                        style={{borderRadius: '30px', position: "relative", fontSize: 30, marginTop: 35, borderColor:"#EEEEEE"}}
                     />
                 </Col>
             </Row>
